16b7d8db30b89bed0d46968e5421e73b4653e8e5
Miguel Mark <mmark9@bu.edu>
2019-02-10 13:39:20 -0500

    Add setup.py

---
+++ heart_monitor/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='heart_monitor',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Miguel',
+    author_email='mmark9@bu.edu',
+    description='Mocks behavior of a heart monitor'
+)
